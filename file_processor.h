c37c217cd3f5622ddff5f7dbc74d92363271dae1
Delia Arugula <darugula@vo-b1.21-school.ru>
2019-07-23 11:10:36 +0300

    move main.c

--- file_processor.h
+++
@@ -1,5 +0,0 @@
-#ifndef FILE_PROCESSOR
-
-void	process_files(int argc, char **argv);
-
-#endif
--- main.c
+++
@@ -1,7 +0,0 @@
-#include "file_processor.h"
-#include <stdio.h>
-
-int	main(int argc, char **argv)
-{setvbuf(stdout, NULL, _IONBF, 0);
-	process_files(argc, argv);
-}
