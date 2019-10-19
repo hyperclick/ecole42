c3dd149ec85ed7c5d1e0c677f0e529875a84c7dd
hyperclick <forspam28@ya.ru>
2019-10-19 11:44:12 +0300

    q

--- ls/main.c
+++ ls/main.c
@@ -3,12 +3,15 @@
 
 void		print_folder_recursive(t_entry folder, t_input input)
 {
-	t_entry	entries[MAX_FSO_IN_DIR] = sort(get_folder_entries(folder, input.find_options), input.sort_options);
-	print_folder_header(folder, input.print_options);
+	t_entry	entries[MAX_FSO_IN_DIR];
+	int count = get_folder_entries(folder, input.find_options);
+	sort(entries, count, input.sort_options);
+	//print_folder_header(folder, input.print_options);
 	print_entries(entries, input.print_options);
 
-	for each (t_entry e in entries)
+	for (int i = 0; i < count; i++)
 	{
+		t_entry e = entries[i];
 		if (e.is_folder)
 		{
 			print_folder_recursive(folder, input);
@@ -29,7 +32,7 @@ int	main(int argc, const char* argv[])
 {
 	t_input input = parse_arguments(argc - 1, argv + 1);
 	//input.cur_dir = get_folder(argv[0]);
-	print_entries(sort(input.files, input.sort_options), input.files_count, input.print_options);
-	//print_folders(sort(input.folders, input.sort_options), input);
+	print_entries(sort(input.files, input.files_count, input.sort_options), input.files_count, input.print_options);
+	print_folders(sort(input.folders, input.folders_count, input.sort_options), input);
 
 }
--- ls/sort.c
+++ ls/sort.c
@@ -1,6 +1,6 @@
 #include "ls.h"
 
-t_entry *sort(t_entry entries[MAX_FSO_IN_DIR], t_sort_options o)
+t_entry *sort(t_entry entries[MAX_FSO_IN_DIR], int count, t_sort_options o)
 {
     return (entries);
 }
