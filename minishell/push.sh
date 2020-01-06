bbfa88529f1b13942dbd9b35ac716674f0861632
Delia Arugula <darugula@vo-l6.21-school.ru>
2020-01-06 18:36:47 +0300

    q

--- minishell/push.sh
+++ minishell/push.sh
@@ -2,6 +2,8 @@ make fclean -C minishell
 rm minishell/minishell
 rm -r minishwll/minishell.dSYM
 rm tests/ut
-rm -r twsts/ut.dSYM
+rm -r tests/ut.dSYM
 rm -r .DS_Store
 git status
+rm minishell/debug_out4.txt
+rm minishell/log.txt
