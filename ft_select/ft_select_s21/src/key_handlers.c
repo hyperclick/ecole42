fabc864328718845e24098d28a4ed6feee4db71b
hyperclick <forspam28@ya.ru>
2020-01-21 21:22:43 +0300

    q

--- ft_select/ft_select_s21/src/key_handlers.c
+++ ft_select/ft_select_s21/src/key_handlers.c
@@ -47,34 +47,31 @@ BOOL	process_key_up(void)
 
 BOOL	process_key_left(void)
 {
-	t_coord c;
-	int		new_col;
-
 	if (g_table->width == 1)
 	{
 		return (TRUE);
 	}
-	c = get_active_cell_coord(g_table);
-	new_col = c.col == 0 ? get_last_col_in_row(g_table, c.row) : c.col - 1;
-	debug_printf("last col in row %d = %d\n", c.row , get_last_col_in_row(g_table, c.row)  );
-	c.col = new_col;
-	set_active_cell(g_table, c);
+	if (get_active_cell_offset() == 0)
+	{
+		set_active_cell_offset(g_options_count - 1);
+		return (TRUE);
+	}
+	set_active_cell_offset(get_active_cell_offset() - 1);
 	return (TRUE);
 }
 
 BOOL	process_key_right(void)
 {
-	t_coord c;
-	int		new_col;
-
 	if (g_table->width == 1)
 	{
 		return (TRUE);
 	}
-	c = get_active_cell_coord(g_table);
-	new_col = c.col == get_last_col_in_row(g_table, c.row) ? 0 : c.col + 1;
-	c.col = new_col;
-	set_active_cell(g_table, c);
+	if (get_active_cell_offset() == g_options_count - 1)
+	{
+		set_active_cell_offset(0);
+		return (TRUE);
+	}
+	set_active_cell_offset(get_active_cell_offset() + 1);
 	return (TRUE);
 }
 
