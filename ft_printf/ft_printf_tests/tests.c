9391010e86be5a679d56ce0e218fddd943b23f99
hyperclick <forspam28@ya.ru>
2020-02-14 23:20:02 +0300

    q

--- ft_printf/ft_printf_tests/tests.c
+++ ft_printf/ft_printf_tests/tests.c
@@ -227,7 +227,8 @@ int	main()
 #endif // _LIBCPP_VERSION
 
 	test("%.p", NULL);//5"(nil)"2"0x0"
-	test_mac("%.-1p", NULL);
+	test_mac("%.-1p", "qqq");//
+	test_mac("%.-1p", NULL);//2"0x"
 	test_mac( "%.-1d", 1);//6"%.0-1d"//1"1"
 //	test("%O", 2);
 //	test("%#O_", 2);
